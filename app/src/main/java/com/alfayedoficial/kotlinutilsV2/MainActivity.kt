e37ec3aa5bcbf4583fb869e66c7acaa2403ca61f
Ali Al Fayed <alialfayed1@gmail.com>
2022-02-04 18:36:28 +0200

    Initial commit

---
+++ app/src/main/java/com/alfayedoficial/kotlinutilsV2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.alfayedoficial.kotlinutilsV2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alfayedoficial.kotlinutils.Utils.kuRunDelayed
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        kuRunDelayed(500){
+
+        }
+    }
+}
